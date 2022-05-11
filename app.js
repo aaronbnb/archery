90ba8fa8b204b7b9f4ff96539cb0ebbe9eced080
Aaron Farber <aaronfarber9@gmail.com>
2022-05-11 13:02:25 -0700

    polishing

--- app.js
+++ app.js
@@ -67,3 +67,26 @@ else {
     event.target.classList.remove('show')
 }
 });
+
+
+document.getElementById('navbarDropdown1').addEventListener('click', event => {
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+})
